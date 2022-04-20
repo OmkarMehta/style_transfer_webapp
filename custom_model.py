aff98e02a4de68fca7c5ee4480242535a1d77cc3
OmkarMehta <mehta.omkar@gmail.com>
2022-04-20 16:11:08 -0500

    models added

---
+++ custom_model.py
@@ -0,0 +1,129 @@
+# Own Architectural Model for the Style Transfer that uses the ideas from GoogleNet, ResNet 
+# After the feedback from Prof, we have decided to add many resnet layers and address the issue of checkerboard artifacts.
+# The reason behind using the instance norm instead of batch norm (https://arxiv.org/abs/1607.08022) is that we would be able to train high-performance architectures 
+# for real-time style transfer (web-app).
+
+import torch 
+
+class CustomModel(torch.nn.Module):
+    def __init__(self):
+        super(CustomModel, self).__init__()
+
+        # Aggresive downsampling, inspired by GoogleNet
+        self.conv1 = ConvLayer(3, 64, kernel_size=7, stride=1)
+        # we will use instance normalization instead of batch normalization, inspired by https://arxiv.org/abs/1607.08022
+        self.norm1 = torch.nn.InstanceNorm2d(64, affine=True)
+        self.conv2 = ConvLayer(64, 64, kernel_size=3, stride=2)
+        self.norm2 = torch.nn.InstanceNorm2d(64, affine=True)
+        self.conv3 = ConvLayer(64, 128, kernel_size=3, stride=2)
+        self.norm3 = torch.nn.InstanceNorm2d(128, affine=True)
+
+        # We will add ResNet blocks to the network, inspired by ResNet paper
+        self.res1 = ResNet(128)
+        self.res2 = ResNet(128)
+        self.res3 = ResNet(128)
+        self.res4 = ResNet(128)
+        self.res5 = ResNet(128)
+        self.res6 = ResNet(128)
+        self.res7 = ResNet(128)
+
+        # Upsampling, inspired by https://distill.pub/2016/deconv-checkerboard/
+        self.deconv1 = UpSampleConv(128, 64, kernel_size=3, stride=1, upsample=2)
+        self.norm4 = torch.nn.InstanceNorm2d(64, affine=True)
+        self.deconv2 = UpSampleConv(64, 64, kernel_size=3, stride=1, upsample=2)
+        self.norm5 = torch.nn.InstanceNorm2d(64, affine=True)
+        self.deconv3 = UpSampleConv(64, 32, kernel_size=3, stride=1, upsample=2)
+        self.norm6 = torch.nn.InstanceNorm2d(32, affine=True)
+        self.deconv4 = UpSampleConv(32, 3, kernel_size=3, stride=1)
+
+        # ReLU activation function
+        self.relu = torch.nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.norm1(self.conv1(x)))
+        x = self.relu(self.norm2(self.conv2(x)))
+        x = self.relu(self.norm3(self.conv3(x)))
+        x = self.res1(x)
+        x = self.res2(x)
+        x = self.res3(x)
+        x = self.res4(x)
+        x = self.res5(x)
+        x = self.res6(x)
+        x = self.res7(x)
+        x = self.relu(self.norm4(self.deconv1(x)))
+        x = self.relu(self.norm5(self.deconv2(x)))
+        x = self.relu(self.norm6(self.deconv3(x)))
+        x = self.deconv4(x)
+        return x
+
+
+
+class ConvLayer(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride):
+        super(ConvLayer, self).__init__()
+        # we have added reflection padding to add padding around the image
+        self.ref_pad = torch.nn.ReflectionPad2d(kernel_size // 2)
+        self.conv2d = torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride)
+
+    def forward(self, x):
+        # get the padded input
+        out = self.ref_pad(x)
+        # get the output of the convolutional layer
+        out = self.conv2d(out)
+        # return the output
+        return out
+
+class ResNet(torch.nn.Module):
+    """ResNet Block
+    Inspired by ResNet paper
+    """
+    def __init__(self, channels):
+        super(ResNet, self).__init__()
+        # first convolutional layer
+        self.conv1 = ConvLayer(channels, channels, kernel_size=3, stride=1)
+        # we will use instance normalization instead of batch normalization, inspired by https://arxiv.org/abs/1607.08022
+        self.in1 = torch.nn.InstanceNorm2d(channels, affine=True)
+        # second convolutional layer
+        self.conv2 = ConvLayer(channels, channels, kernel_size=3, stride=1)
+        # we will use instance normalization instead of batch normalization, inspired by https://arxiv.org/abs/1607.08022
+        self.in2 = torch.nn.InstanceNorm2d(channels, affine=True)
+        # ReLU activation function
+        self.relu = torch.nn.ReLU()
+
+    def forward(self, x):
+        # store x as a residual
+        residual = x
+        # get the output of the first convolutional layer
+        out = self.relu(self.in1(self.conv1(x)))
+        # get the output of the second convolutional layer
+        out = self.in2(self.conv2(out))
+        # add the residual to the output
+        out = out + residual
+        # return the output
+        return out
+
+class UpSampleConv(torch.nn.Module):
+    '''
+    Inspired from http://distill.pub/2016/deconv-checkerboard/
+    '''
+    def __init__(self, in_channels, out_channels, kernel_size, stride, upsample=None):
+        super(UpSampleConv, self).__init__()
+        # get upsample 
+        self.upsample = upsample
+        # we will be using reflection padding to add zeros around the image
+        self.ref_pad = torch.nn.ReflectionPad2d(kernel_size // 2)
+        # define the convolutional layer
+        self.conv = torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride)
+
+    def forward(self, x):
+        input_x = x # save the input
+        # upsample the input
+        if self.upsample:
+            # interpolate the input using nearest neighbor interpolation
+            input_x = torch.nn.functional.interpolate(input_x, mode='nearest', scale_factor=self.upsample)
+        # get the padded input
+        padded_x = self.ref_pad(input_x)
+        # get the output of the convolutional layer
+        output_x = self.conv(padded_x)
+        # return the output
+        return output_x
--- vgg16.py
+++ vgg16.py
@@ -1,48 +1,133 @@
-import torch 
-from torchvision import models # import the pre-trained model
-from collections import namedtuple # import namedtuple for the namedtuple
+# VGG16 as style transfer model 
+import torch
+from torchvision import models
 
 class VGG16(torch.nn.Module):
-    def __init__(self, requires_grad=False):
+    def __init__(self, requires_grad=True):
         super(VGG16, self).__init__()
         # load the pre-trained model and get features
         self.vgg = models.vgg16(pretrained=True).features
-        # we are dividing the whole architecture into four parts: each of which has a relu output as the last layer
-        self.group1 = torch.nn.Sequential()
-        # add the  modules of the first group
-        for i in range(4):
-            self.group1.add_module(str(i), self.vgg[i])
-        self.group2 = torch.nn.Sequential()
-        # add the modules of the second group
-        for i in range(4, 9):
-            self.group2.add_module(str(i), self.vgg[i])
-        self.group3 = torch.nn.Sequential()
-        # add the modules of the third group
-        for i in range(9, 16):
-            self.group3.add_module(str(i), self.vgg[i])
-        self.group4 = torch.nn.Sequential()
-        # add the modules of the fourth group
-        for i in range(16, 23):
-            self.group4.add_module(str(i), self.vgg[i])
-        # if gradients are not required, update parameters to have no gradients
-        if not requires_grad:
+
+        # add all the layers of vgg16
+        self.conv1_1 = torch.nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
+        # load the features of the first layer
+        self.conv1_1.weight.data = self.vgg[0].weight.data
+        self.conv1_2 = torch.nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        # load the features of the second layer
+        self.conv1_2.weight.data = self.vgg[2].weight.data
+        self.pool1 = torch.nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv2_1 = torch.nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        # load the features of the third layer
+        self.conv2_1.weight.data = self.vgg[5].weight.data
+        self.conv2_2 = torch.nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
+        # load the features of the fourth layer
+        self.conv2_2.weight.data = self.vgg[7].weight.data
+        self.pool2 = torch.nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv3_1 = torch.nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        # load the features of the fifth layer
+        self.conv3_1.weight.data = self.vgg[10].weight.data
+        self.conv3_2 = torch.nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        # load the features of the sixth layer
+        self.conv3_2.weight.data = self.vgg[12].weight.data
+        self.conv3_3 = torch.nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        # load the features of the seventh layer
+        self.conv3_3.weight.data = self.vgg[14].weight.data
+        self.pool3 = torch.nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv4_1 = torch.nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1)
+        # load the features of the eighth layer
+        self.conv4_1.weight.data = self.vgg[17].weight.data
+        self.conv4_2 = torch.nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        # load the features of the ninth layer
+        self.conv4_2.weight.data = self.vgg[19].weight.data
+        self.conv4_3 = torch.nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        # load the features of the tenth layer
+        self.conv4_3.weight.data = self.vgg[21].weight.data
+        self.pool4 = torch.nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv5_1 = torch.nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        # load the features of the eleventh layer
+        self.conv5_1.weight.data = self.vgg[24].weight.data
+        self.conv5_2 = torch.nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        # load the features of the twelfth layer
+        self.conv5_2.weight.data = self.vgg[26].weight.data
+        self.conv5_3 = torch.nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        # load the features of the thirteenth layer
+        self.conv5_3.weight.data = self.vgg[28].weight.data
+        self.pool5 = torch.nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.relu = torch.nn.ReLU()
+
+        # Up-sampling layers
+        self.deconv1 = UpSampleConv(512, 256, kernel_size=3, stride=1, upsample=2)
+        self.deconv2 = UpSampleConv(256, 128, kernel_size=3, stride=1, upsample=2)
+        self.deconv3 = UpSampleConv(128, 64, kernel_size=3, stride=1, upsample=2)
+        self.deconv4 = UpSampleConv(64, 32, kernel_size=3, stride=1, upsample=2)
+        self.deconv5 = UpSampleConv(32, 3, kernel_size=3, stride=1)
+
+        # if gradients are required, update parameters to have gradients
+        if requires_grad:
             for param in self.parameters():
-                param.requires_grad = False
+                param.requires_grad = True
+    
     def forward(self, x):
-        # get the output of the first group
-        x1 = self.group1(x)
-        # get the output of the second group
-        x2 = self.group2(x1)
-        # get the output of the third group
-        x3 = self.group3(x2)
-        # get the output of the fourth group
-        x4 = self.group4(x3)
-        # create a namedtuple to store the output of the four groups
-        out_groups = namedtuple('out_groups', ['x1', 'x2', 'x3', 'x4'])
-        # get the output of the four groups
-        out = out_groups(x1, x2, x3, x4)
-        # return the output of the four groups
-        return out
+        x = self.relu(self.conv1_1(x))
+        x = self.relu(self.conv1_2(x))
+        x = self.pool1(x)
+        x = self.relu(self.conv2_1(x))
+        x = self.relu(self.conv2_2(x))
+        x = self.pool2(x)
+        x = self.relu(self.conv3_1(x))
+        x = self.relu(self.conv3_2(x))
+        x = self.relu(self.conv3_3(x))
+        x = self.pool3(x)
+        x = self.relu(self.conv4_1(x))
+        x = self.relu(self.conv4_2(x))
+        x = self.relu(self.conv4_3(x))
+        x = self.pool4(x)
+        x = self.relu(self.conv5_1(x))
+        x = self.relu(self.conv5_2(x))
+        x = self.relu(self.conv5_3(x))
+        x = self.pool5(x)
+        # upsample the features
+        x = self.deconv1(x)
+        x = self.deconv2(x)
+        x = self.deconv3(x)
+        x = self.deconv4(x)
+        x = self.deconv5(x)
+        return x
 
 
 
+
+class UpSampleConv(torch.nn.Module):
+    '''
+    Inspired from http://distill.pub/2016/deconv-checkerboard/
+    '''
+    def __init__(self, in_channels, out_channels, kernel_size, stride, upsample=None):
+        super(UpSampleConv, self).__init__()
+        # get upsample 
+        self.upsample = upsample
+        # we will be using reflection padding to add zeros around the image
+        self.ref_pad = torch.nn.ReflectionPad2d(kernel_size // 2)
+        # define the convolutional layer
+        self.conv = torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride)
+
+    def forward(self, x):
+        input_x = x # save the input
+        # upsample the input
+        if self.upsample:
+            # interpolate the input using nearest neighbor interpolation
+            input_x = torch.nn.functional.interpolate(input_x, mode='nearest', scale_factor=self.upsample)
+        # get the padded input
+        padded_x = self.ref_pad(input_x)
+        # get the output of the convolutional layer
+        output_x = self.conv(padded_x)
+        # return the output
+        return output_x
+
+
+        
+    
