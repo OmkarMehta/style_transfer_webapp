80822aa00088556ab732e2777bc9520c3131fb04
OmkarMehta <mehta.omkar@gmail.com>
2022-04-28 20:20:03 -0500

    trained on tiny imagenet but cuda memeory issue

---
+++ style-trasfer-from-scratch/milestone2/download_data.sh
@@ -0,0 +1,7 @@
+wget -nc http://cs231n.stanford.edu/tiny-imagenet-200.zip
+unzip tiny-imagenet-200.zip
+rm -r ./tiny-imagenet-200/test
+python3 val_format.py
+find . -name "*.txt" -delete
+cp -r tiny-imagenet-200 tiny-224
+python3 resize.py
